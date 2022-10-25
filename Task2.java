354532c1fae3375ac7d3e2861d7d6f3a2fcfabf6
YUNLONG892a <116412023+YUNLONG892a@users.noreply.github.com>
2022-10-25 09:48:38 +0800

    Add files via upload

--- Task2.java
+++ Task2.java
@@ -74,29 +74,24 @@ public class Task2 {
            String m = str[j];
                Pattern p = Pattern.compile("[^a-z]switch[^a-z]");
                Matcher match = p.matcher(m);
-         	   if(match.find()) {
+               Pattern p1 = Pattern.compile("[^a-z]case[^a-z]");
+               Matcher match1 = p1.matcher(m);
+               Pattern p2 = Pattern.compile("[^a-z]default[^a-z]");
+               Matcher match2 = p2.matcher(m);
+         	  if(match.find()) {
          		    switchnum++;
-         		    for(int i = j + 1;i <= n - 1 ; i++) {
-         		    	m = str[i];
-         		    	Pattern p1 = Pattern.compile("[^a-z]case[^a-z]");
-                        Matcher match1 = p1.matcher(m);
-                        Pattern p2 = Pattern.compile("[^a-z]default[^a-z]");
-                        Matcher match2 = p2.matcher(m);
-                        if(match1.find()) {
-                        	casenum++;
-                        }
-                        else if(match2.find()) {
-                        	num[switchnum - 1] = casenum;
-                        	casenum = 0;
-                        	j = i + 1;
-                        	break;
-                        }
-                        
-         		    } 	
-                    
-               }                     
+         		    continue;
+         	 }
+         	  if(match1.find()) {
+              	casenum++;
+              }
+              else if(match2.find()) {
+              	num[switchnum - 1] = casenum;
+              	casenum = 0;
+              }   
+             }                     
 	        
-	    }
+	    
         System.out.println("switch num: " + switchnum);
         System.out.print("case num: " );
         for(int i = 0 ; i <= switchnum - 1 ; i++) {
@@ -109,7 +104,7 @@ public class Task2 {
 	public static void level34(String str[],int n) {
 		int ifnum = 0;
 		int it = 0;
-		boolean [] num = new boolean [10000];
+		boolean [] bool = new boolean [10000];
 		int ienum = 0;
 		int ieenum = 0;
         for(int  j = 0 ; j <= n - 1; j++) {
@@ -124,20 +119,20 @@ public class Task2 {
          	   if(a&&!b) {
          		  
          		    ifnum++;
-         		    num[it] = false;
+         		    bool[it] = false;
          		    it++;
          	   }
          	   else if(a&&b) {
-         		    num[it - 1] = true;
+         		    bool[it - 1] = true;
          	   }
          	  else if(!a&&b) {
-       		        if(num[it - 1]) {
+       		        if(bool[it - 1]) {
        		        	ieenum++;
        		        }
        		        else {
        		        	ienum++;
        		        }
-       		        num[it - 1] = false;
+       		        bool[it - 1] = false;
        		        it--;
          	  }
          		    
